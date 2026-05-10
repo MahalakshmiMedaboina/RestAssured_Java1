43ce82ba4fcd0be7dd234f7d251b41bd904ff4bc
MahalakshmiMedaboina <medaboinamahaa@gmail.com>
2026-05-10 23:32:00 +0530

    created a separate package :files created payload to keep all JSON at one place

--- src/Basics.java
+++ src/Basics.java
@@ -2,6 +2,8 @@ import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import files.payload;
+
 public class Basics {
 
 	public static void main(String[] args) {
@@ -14,23 +16,7 @@ public class Basics {
 		RestAssured.baseURI= "https://rahulshettyacademy.com";
 		
 		given().log().all().queryParam("Key", "qaclick123").header("Content-Type","application/json")
-			.body("{\r\n"
-					+ "  \"location\": {\r\n"
-					+ "    \"lat\": -38.383494,\r\n"
-					+ "    \"lng\": 33.427362\r\n"
-					+ "  },\r\n"
-					+ "  \"accuracy\": 50,\r\n"
-					+ "  \"name\": \"RSA_Mahaa12\",\r\n"
-					+ "  \"phone_number\": \"(+91) 666 222 1234\",\r\n"
-					+ "  \"address\": \"1, side layout, cohen 09\",\r\n"
-					+ "  \"types\": [\r\n"
-					+ "    \"shoe park\",\r\n"
-					+ "    \"shop\"\r\n"
-					+ "  ],\r\n"
-					+ "  \"website\": \"https://rahulshettyacademy.com/\",\r\n"
-					+ "  \"language\": \"English-IN\"\r\n"
-					+ "}\r\n"
-					+ "").when().post("/maps/api/place/add/json")
+			.body(payload.AddPlace()).when().post("/maps/api/place/add/json")
 			.then().log().all().assertThat().statusCode(200)
 			.body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)");
 
